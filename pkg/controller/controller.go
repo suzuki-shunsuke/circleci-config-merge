d871ce326cd7ccbd91dd04769db871eff12065ff
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-11-03 14:19:00 +0900

    feat: sort workflow jobs by job name
    It is helpful to compare generated config

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sort"
 
 	"gopkg.in/yaml.v2"
 )
@@ -72,6 +73,58 @@ type Workflow struct {
 	Triggers interface{}            `yaml:",omitempty"`
 	Jobs     []interface{}          `yaml:",omitempty"`
 	others   map[string]interface{} `yaml:",omitempty"`
+}
+
+func getJobName(job interface{}) (string, error) {
+	switch v := job.(type) {
+	case string:
+		return v, nil
+	case map[interface{}]interface{}:
+		for jobName, jobValue := range v {
+			a, ok := jobValue.(map[interface{}]interface{})
+			if !ok {
+				return "", fmt.Errorf("workflow job's element must be map: %+v", jobValue)
+			}
+			if name, ok := a["name"]; ok {
+				jobName = name
+			}
+			s, ok := jobName.(string)
+			if !ok {
+				return "", fmt.Errorf("workflow job's name must be string: %+v", jobName)
+			}
+			return s, nil
+		}
+		return "", fmt.Errorf("workflow job's element is empty")
+	default:
+		return "", fmt.Errorf("workflow job must be string or map")
+	}
+}
+
+func sortJobs(jobs []interface{}) ([]interface{}, error) {
+	type WorkflowJob struct {
+		Name string
+		Job  interface{}
+	}
+	wfJobs := make([]WorkflowJob, len(jobs))
+	for i, job := range jobs {
+		name, err := getJobName(job)
+		if err != nil {
+			return nil, fmt.Errorf("get a job name: %w", err)
+		}
+		wfJobs[i] = WorkflowJob{
+			Name: name,
+			Job:  job,
+		}
+	}
+
+	sort.Slice(wfJobs, func(i, j int) bool {
+		return wfJobs[i].Name < wfJobs[j].Name
+	})
+	arr := make([]interface{}, len(jobs))
+	for i, job := range wfJobs {
+		arr[i] = job.Job
+	}
+	return arr, nil
 }
 
 func (wf Workflow) MarshalYAML() (interface{}, error) {
@@ -197,6 +250,13 @@ func (ctrl Controller) Run(ctx context.Context, params Params) error {
 			return fmt.Errorf("read a file"+filePath+": %w", err)
 		}
 		cfg = ctrl.mergeConfig(cfg, m)
+	}
+	for k, workflow := range cfg.Workflows.Workflows {
+		jobs, err := sortJobs(workflow.Jobs)
+		if err == nil {
+			workflow.Jobs = jobs
+		}
+		cfg.Workflows.Workflows[k] = workflow
 	}
 	if err := yaml.NewEncoder(ctrl.Stdout).Encode(cfg); err != nil {
 		return fmt.Errorf("encode a merged config as YAML: %w", err)
